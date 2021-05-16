c46c53cf9e6c8583a123724a2955a2a07d322ebc
Brandon Tom <magicink@gmail.com>
2021-05-16 09:58:07 -0700

    Experimenting with new room generation algorithms

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -1,19 +1,69 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TileController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TileController origin;
+    [SerializeField] private List<Connector> connectors;
+
+    public TileController Origin
     {
-        
+        get => origin;
+        set => origin = value;
+    }
+    
+    public List<Connector> Connectors
+    {
+        get => connectors;
+        private set => connectors = value;
     }
 
-    // Update is called once per frame
-    void Update()
+    public delegate void OnCreation(TileController tileController);
+    public OnCreation HandleCreation;
+    
+    public bool CollisionDetected { get; set; }
+    
+    private bool _checkedForCollisions;
+    private BoxCollider _boxCollider;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
     {
-        
+        _boxCollider = gameObject.AddComponent<BoxCollider>();
+        _boxCollider.isTrigger = true;
+        _rigidbody = gameObject.AddComponent<Rigidbody>();
+        _rigidbody.useGravity = false;
+        _rigidbody.isKinematic = true;
+        GetConnectors();
+    }
+
+    public void Start()
+    {
+        var collisions = Physics.OverlapBox(transform.position, _boxCollider.size, Quaternion.identity, LayerMask.GetMask("Tile"));
+        if (collisions.Length <= 0) return;
+        foreach (var collision in collisions)
+        {
+            if (collision.gameObject != gameObject)
+            {
+                if (Origin == null)
+                {
+                    CollisionDetected = true;
+                }
+                else
+                {
+                    if (collision.gameObject != Origin.gameObject)
+                    {
+                        CollisionDetected = true;
+                    }
+                }
+            }
+        }
+    }
+
+    public List<Connector> GetConnectors()
+    {
+        Connectors = GetComponentsInChildren<Connector>().Where(c => !c.Connected).ToList();
+        return Connectors;
     }
 }
